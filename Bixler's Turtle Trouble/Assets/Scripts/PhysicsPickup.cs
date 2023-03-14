a52f53b8836fbe4cc27a9e7ef9a350751a9b9e89
Ryanlange05 <116192483+Ryanlange05@users.noreply.github.com>
2023-03-14 10:17:56 -0400

    added physics, part of pick up system and temporary crosshair

--- Bixler's Turtle Trouble/Assets/Scripts/PhysicsPickup.cs
+++ Bixler's Turtle Trouble/Assets/Scripts/PhysicsPickup.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PhysicsPickup : MonoBehaviour
 {
+    
+    [SerializeField] private LayerMask PickupMask; // Determines what layer the player is able to pick up
+    [SerializeField] private Camera PlayerCamera; // Allows function viewport point to ray
+    [SerializeField] private Transform PickupTarget;
+    [Space]
+    [SerializeField] private float PickupRange;
+    private Rigidbody CurrentObject;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,33 @@ public class PhysicsPickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.E)) 
+        {
         
+            if(CurrentObject)
+            {
+                CurrentObject.useGravity = true;
+                CurrentObject = null;
+                return;
+            }
+
+            Ray CameraRay = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            if(Physics.Raycast(CameraRay, out RaycastHit HitInfo, PickupRange, PickupMask))
+            {
+                CurrentObject = HitInfo.rigidbody;
+                CurrentObject.useGravity = false;
+            }
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        if(CurrentObject)
+        {
+            Vector3 DirectionToPoint = PickupTarget.position - CurrentObject.position;
+            float DistanceToPoint = DirectionToPoint.magnitude;
+
+            CurrentObject.velocity = DirectionToPoint * 12f * DistanceToPoint;
+        }
     }
 }
