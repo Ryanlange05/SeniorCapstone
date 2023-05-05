75844c0fd29439d81f07490f63d2459265f5e7ca
Ryanlange05 <116192483+Ryanlange05@users.noreply.github.com>
2023-05-05 10:21:26 -0400

    dead screen is workin

--- Bixler's Turtle Trouble/Assets/Scripts/MainMenu.cs
+++ Bixler's Turtle Trouble/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,16 @@ public class MainMenu : MonoBehaviour{
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
     public void QuitGame()
     {
         Debug.Log("QUIT!");
